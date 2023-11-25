6c0e4e7add7dd263d2a382439ae53930ebc096f5
Ayi <58356542+Ayi1337@users.noreply.github.com>
2023-11-26 05:23:24 +0800

    Update merge-gold.js

--- igoldaccount/curposition/merge-gold.js
+++ igoldaccount/curposition/merge-gold.js
@@ -1,15 +1,24 @@
 // 自定义变量
-let customMergeBal = 500; // 克数
-let goldPricePerGram = 454; // 克单价
+let customMergeBal = 500.0163; // 克数
+let goldPricePerGram = 454; // 每克黄金价格
+let benefitRate = 0.1145141; // 利润率
 
 let response = JSON.parse($response.body);
 
-if (response && response["bizResult"] && response["bizResult"]["data"] && response["bizResult"]["data"]["goldList"]) {
-    let goldList = response["bizResult"]["data"]["goldList"];
-    for (let i = 0; i < goldList.length; i++) {
-        goldList[i]["mergeBal"] = customMergeBal.toString();
-        goldList[i]["mergeAmt"] = (customMergeBal * goldPricePerGram).toFixed(2);
+if (response && response["bizResult"] && response["bizResult"]["data"]) {
+    let data = response["bizResult"]["data"];
+    let goldList = data["goldList"];
+    
+    if (goldList) {
+        for (let i = 0; i < goldList.length; i++) {
+            goldList[i]["mergeBal"] = customMergeBal.toString();
+            goldList[i]["mergeAmt"] = (customMergeBal * goldPricePerGram).toFixed(2);
+            goldList[i]["benefit"] = (customMergeBal * goldPricePerGram * benefitRate).toFixed(2);
+        }
     }
+
+    // 更新 mergeTotalAmt
+    data["mergeTotalAmt"] = (customMergeBal * goldPricePerGram).toFixed(2);
 }
 
 $done({body: JSON.stringify(response)});
