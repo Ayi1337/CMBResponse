44bd8edfc904170e3c6caa0a6540ed6a34356d0d
Ayi <58356542+Ayi1337@users.noreply.github.com>
2023-11-26 05:36:28 +0800

    Update QueryGoldAjax.js

--- QueryGoldAjax/QueryGoldAjax.js
+++ QueryGoldAjax/QueryGoldAjax.js
@@ -1,27 +1,26 @@
-let response = JSON.parse($response.body);
+// 定义固定参数
+let customMergeBal = 500.0163; // 克数
+let goldPricePerGram = 454; // 每克黄金价格
+let benefitRate = 0.1145141; // 利润率
 
-// 定义 sumBalance 和 Balance 的增加比例
-const balanceIncreasePercentage = 12.23541 / 100;
-// 定义 Income 的增加比例
-const incomeIncreasePercentage = 11.451419 / 100;
-const originalBalance = 100000.00;  // 初始余额
+let response = JSON.parse($response.body);
 
 if (response && response["$SysResult$"] && response["$SysResult$"]["$Content$"]) {
     let content = JSON.parse(response["$SysResult$"]["$Content$"]);
     if (content && content["Result"]) {
-        if (content["Result"]["sumBalance"] !== undefined) {
-            // 修改 sumBalance 并应用固定百分比，结果限定为两位小数
-            content["Result"]["sumBalance"] = parseFloat((originalBalance + originalBalance * balanceIncreasePercentage).toFixed(2));
-        }
+        let calculatedAmt = (customMergeBal * goldPricePerGram).toFixed(2);
+        let calculatedIncome = (customMergeBal * goldPricePerGram * benefitRate).toFixed(2);
+
+        // 更新 sumBalance 和 Balance
+        content["Result"]["sumBalance"] = parseFloat(calculatedAmt);
         if (content["Result"]["accountGold"]) {
             for (let key in content["Result"]["accountGold"]) {
                 if (content["Result"]["accountGold"][key]["Balance"] !== undefined) {
-                    // 修改 Balance 并应用固定百分比，结果限定为两位小数
-                    content["Result"]["accountGold"][key]["Balance"] = parseFloat((originalBalance + originalBalance * balanceIncreasePercentage).toFixed(2));
+                    content["Result"]["accountGold"][key]["Balance"] = parseFloat(calculatedAmt);
                 }
-                // 修改 Income 为 originalBalance * 11.451419%，结果限定为两位小数
+                // 更新 Income
                 if (content["Result"]["accountGold"][key]["Income"] !== undefined) {
-                    content["Result"]["accountGold"][key]["Income"] = parseFloat((originalBalance * incomeIncreasePercentage).toFixed(2));
+                    content["Result"]["accountGold"][key]["Income"] = parseFloat(calculatedIncome);
                 }
             }
         }
