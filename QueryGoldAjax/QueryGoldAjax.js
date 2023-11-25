e714907790a6d8f7974cb5f2cf650e9369002e9c
Ayi <58356542+Ayi1337@users.noreply.github.com>
2023-11-26 04:38:07 +0800

    Update QueryGoldAjax.js
    fix

--- QueryGoldAjax/QueryGoldAjax.js
+++ QueryGoldAjax/QueryGoldAjax.js
@@ -2,23 +2,23 @@ let response = JSON.parse($response.body);
 
 // 定义增加的比例
 const increasePercentage = 12.23541 / 100;
+const originalBalance = 100000.00;  // 初始余额
 
 if (response && response["$SysResult$"] && response["$SysResult$"]["$Content$"]) {
     let content = JSON.parse(response["$SysResult$"]["$Content$"]);
     if (content && content["Result"]) {
         if (content["Result"]["sumBalance"] !== undefined) {
             // 修改 sumBalance 并应用固定百分比
-            let originalBalance = content["Result"]["sumBalance"];
             content["Result"]["sumBalance"] = originalBalance + originalBalance * increasePercentage;
         }
         if (content["Result"]["accountGold"]) {
             for (let key in content["Result"]["accountGold"]) {
                 if (content["Result"]["accountGold"][key]["Balance"] !== undefined) {
                     // 修改 Balance 并应用固定百分比
-                    let originalBalance = content["Result"]["accountGold"][key]["Balance"];
                     content["Result"]["accountGold"][key]["Balance"] = originalBalance + originalBalance * increasePercentage;
-
-                    // 同样修改 Income
+                }
+                // 修改 Income 并应用固定百分比
+                if (content["Result"]["accountGold"][key]["Income"] !== undefined) {
                     content["Result"]["accountGold"][key]["Income"] = originalBalance * increasePercentage;
                 }
             }
