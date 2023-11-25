3b2561c4229fb9869dc9b4e6aa7d699e73528192
Ayi <58356542+Ayi1337@users.noreply.github.com>
2023-11-26 04:16:29 +0800

    Update QueryGoldAjax.js

--- QueryGoldAjax/QueryGoldAjax.js
+++ QueryGoldAjax/QueryGoldAjax.js
@@ -4,12 +4,12 @@ if (response && response["$SysResult$"] && response["$SysResult$"]["$Content$"])
     let content = JSON.parse(response["$SysResult$"]["$Content$"]);
     if (content && content["Result"]) {
         if (content["Result"]["sumBalance"] !== undefined) {
-            content["Result"]["sumBalance"] = 1000.00;  // 修改 sumBalance
+            content["Result"]["sumBalance"] = 100000.00;  // 修改 sumBalance
         }
         if (content["Result"]["accountGold"]) {
             for (let key in content["Result"]["accountGold"]) {
                 if (content["Result"]["accountGold"][key]["Balance"] !== undefined) {
-                    content["Result"]["accountGold"][key]["Balance"] = 1000.00;  // 修改 Balance
+                    content["Result"]["accountGold"][key]["Balance"] = 100000.00;  // 修改 Balance
                 }
             }
         }
