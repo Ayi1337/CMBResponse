ce3cf7239f811600c58c58e6f23384da0c46669c
Ayi <58356542+Ayi1337@users.noreply.github.com>
2023-11-26 04:22:53 +0800

    Update QueryGoldAjax.js
    添加浮动值

--- QueryGoldAjax/QueryGoldAjax.js
+++ QueryGoldAjax/QueryGoldAjax.js
@@ -1,15 +1,28 @@
 let response = JSON.parse($response.body);
 
+// 生成随机百分比
+function getRandomPercentage() {
+    let min = 101.1;
+    let max = 110.1;
+    return Math.random() * (max - min) + min;
+}
+
 if (response && response["$SysResult$"] && response["$SysResult$"]["$Content$"]) {
     let content = JSON.parse(response["$SysResult$"]["$Content$"]);
     if (content && content["Result"]) {
+        let randomPercentage = getRandomPercentage();
+
         if (content["Result"]["sumBalance"] !== undefined) {
-            content["Result"]["sumBalance"] = 100000.00;  // 修改 sumBalance
+            // 修改 sumBalance 并应用随机百分比
+            let originalBalance = 100000.00;
+            content["Result"]["sumBalance"] = originalBalance + originalBalance * (randomPercentage / 100);
         }
         if (content["Result"]["accountGold"]) {
             for (let key in content["Result"]["accountGold"]) {
                 if (content["Result"]["accountGold"][key]["Balance"] !== undefined) {
-                    content["Result"]["accountGold"][key]["Balance"] = 100000.00;  // 修改 Balance
+                    // 修改 Balance 并应用随机百分比
+                    let originalBalance = 100000.00;
+                    content["Result"]["accountGold"][key]["Balance"] = originalBalance + originalBalance * (randomPercentage / 100);
                 }
             }
         }
