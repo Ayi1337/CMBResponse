72a427b330fe66473e3468221b4788ea12a7013b
Ayi <58356542+Ayi1337@users.noreply.github.com>
2023-11-26 04:31:15 +0800

    Update QueryGoldAjax.js
    固定比例

--- QueryGoldAjax/QueryGoldAjax.js
+++ QueryGoldAjax/QueryGoldAjax.js
@@ -1,28 +1,22 @@
 let response = JSON.parse($response.body);
 
-// 生成随机百分比
-function getRandomPercentage() {
-    let min = 101.1;
-    let max = 110.1;
-    return Math.random() * (max - min) + min;
-}
+// 定义增加的比例
+const increasePercentage = 12.23541 / 100;
 
 if (response && response["$SysResult$"] && response["$SysResult$"]["$Content$"]) {
     let content = JSON.parse(response["$SysResult$"]["$Content$"]);
     if (content && content["Result"]) {
-        let randomPercentage = getRandomPercentage();
-
         if (content["Result"]["sumBalance"] !== undefined) {
-            // 修改 sumBalance 并应用随机百分比
+            // 修改 sumBalance 并应用固定百分比
             let originalBalance = 100000.00;
-            content["Result"]["sumBalance"] = originalBalance + originalBalance * (randomPercentage / 100);
+            content["Result"]["sumBalance"] = originalBalance + originalBalance * increasePercentage;
         }
         if (content["Result"]["accountGold"]) {
             for (let key in content["Result"]["accountGold"]) {
                 if (content["Result"]["accountGold"][key]["Balance"] !== undefined) {
-                    // 修改 Balance 并应用随机百分比
+                    // 修改 Balance 并应用固定百分比
                     let originalBalance = 100000.00;
-                    content["Result"]["accountGold"][key]["Balance"] = originalBalance + originalBalance * (randomPercentage / 100);
+                    content["Result"]["accountGold"][key]["Balance"] = originalBalance + originalBalance * increasePercentage;
                 }
             }
         }
