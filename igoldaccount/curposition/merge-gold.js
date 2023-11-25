4d61ca70729c7357c85cd1488d699b134085bc00
Ayi <58356542+Ayi1337@users.noreply.github.com>
2023-11-26 05:36:49 +0800

    Update merge-gold.js

--- igoldaccount/curposition/merge-gold.js
+++ igoldaccount/curposition/merge-gold.js
@@ -1,4 +1,3 @@
-// 自定义变量
 let customMergeBal = 500.0163; // 克数
 let goldPricePerGram = 454; // 每克黄金价格
 let benefitRate = 0.1145141; // 利润率
@@ -10,15 +9,18 @@ if (response && response["bizResult"] && response["bizResult"]["data"]) {
     let goldList = data["goldList"];
     
     if (goldList) {
+        let calculatedAmt = (customMergeBal * goldPricePerGram).toFixed(2);
+        let calculatedBenefit = (customMergeBal * goldPricePerGram * benefitRate).toFixed(2);
+
         for (let i = 0; i < goldList.length; i++) {
             goldList[i]["mergeBal"] = customMergeBal.toString();
-            goldList[i]["mergeAmt"] = (customMergeBal * goldPricePerGram).toFixed(2);
-            goldList[i]["benefit"] = (customMergeBal * goldPricePerGram * benefitRate).toFixed(2);
+            goldList[i]["mergeAmt"] = calculatedAmt;
+            goldList[i]["benefit"] = calculatedBenefit;
         }
-    }
 
-    // 更新 mergeTotalAmt
-    data["mergeTotalAmt"] = (customMergeBal * goldPricePerGram).toFixed(2);
+        // 更新 mergeTotalAmt
+        data["mergeTotalAmt"] = calculatedAmt;
+    }
 }
 
 $done({body: JSON.stringify(response)});
