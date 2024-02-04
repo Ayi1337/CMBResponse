88616a5939dbebfaec98b6cd37a963ce390f9f93
Ayi <58356542+Ayi1337@users.noreply.github.com>
2024-02-04 13:29:13 +0800

    Update card-view-query.js

--- test/v1/daccountview/fund-product/card-view-query.js
+++ test/v1/daccountview/fund-product/card-view-query.js
@@ -1,9 +1,21 @@
-let specifiedValue = 123456.00;
+let specifiedValue = 1100000.00;
 
 let response = JSON.parse($response.body);
 
 if (response && response["bizResult"] && response["bizResult"]["data"] && response["bizResult"]["data"]["fundResult"] && response["bizResult"]["data"]["fundResult"]["data"]) {
-    response["bizResult"]["data"]["fundResult"]["data"]["sumAsset"] = specifiedValue;
+    let fundData = response["bizResult"]["data"]["fundResult"]["data"];
+
+    fundData["sumAsset"] = specifiedValue;
+
+    if (fundData["fundMap"]) {
+        for (let key in fundData["fundMap"]) {
+            if (fundData["fundMap"].hasOwnProperty(key)) {
+                fundData["fundMap"][key] = specifiedValue;
+            }
+        }
+    }
+
+    response["bizResult"]["data"]["fundResult"]["data"] = fundData;
 }
 
 $done({body: JSON.stringify(response)});
